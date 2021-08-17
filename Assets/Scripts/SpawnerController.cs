3e927b363f8de84a8ad76b6d1fe06b63844673fd
Juan Mejía <juanes.reich@gmail.com>
2021-08-17 04:32:41 -0500

    Final commit for today

--- Assets/Scripts/SpawnerController.cs
+++ Assets/Scripts/SpawnerController.cs
@@ -1,24 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnerController : MonoBehaviour
 {
-    [SerializeField] private Transform[] spawnerLocations;
-    [SerializeField] private GameObject[] prefabsToSpawn;
-    [SerializeField] private GameObject[] prefabsToClone; 
+    // [SerializeField] private Transform[] spawnerLocations;
+    // [SerializeField] private GameObject[] prefabsToSpawn;
+    // [SerializeField] private GameObject[] prefabsToClone;
+    
+    [SerializeField] private bool playerDetected;
+
+    private void Awake()
+    {
+    }
+    
     private void Start()
     {
-        prefabsToClone = new GameObject[prefabsToSpawn.Length];
-        Spawn();
+        //prefabsToClone = new GameObject[prefabsToSpawn.Length];
+        //Spawn();
     }
 
-    private void Spawn()
+    /*private void Spawn()
     {
         for (var i = 0; i < prefabsToSpawn.Length; i++)
         {
             prefabsToClone[i] = Instantiate(prefabsToSpawn[i], spawnerLocations[i].transform.position,
-                Quaternion.Euler(0, 0, 0));
+                Quaternion.Euler(0, 0, 0) )as GameObject;
         }
     }
 
@@ -38,5 +46,24 @@ public class SpawnerController : MonoBehaviour
         
         // Spawn all game objects
         Spawn();
+    }*/
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Doggy");
+        if (other.tag == "Player")
+        {
+            //playerDetected = true;
+            Debug.Log("wolfy");
+        }
     }
+
+    private void OnTriggerExit(Collider other) //DangerZone detection
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerDetected = false;
+        }
+    }
+    
 }
