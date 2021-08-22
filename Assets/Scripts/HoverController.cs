6719aa5a3192d2b6f1ea7f2e5d900a81262c4724
Juan Mejía <juanes.reich@gmail.com>
2021-08-22 02:26:37 -0500

    las commit for today

--- Assets/Scripts/HoverController.cs
+++ Assets/Scripts/HoverController.cs
@@ -2,91 +2,112 @@
 using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(InputHandler))]
 public class HoverController : MonoBehaviour
 {
-    private InputHandler _input;
-    [SerializeField] private float GoUpForce = 12500f;
-    [SerializeField] private float ForwardForce = 20000f;
-    [SerializeField] private float RotationTorque = 10000f;
-    
-    [SerializeField] private float rotationSpeed = 2f;
-    
-    [SerializeField] private Transform[] RaycastHelpers;
-    [SerializeField] private Transform CenterRaycastHelper;
-    [SerializeField] private float HeightFromGround = 2f;
-    [SerializeField] private float GroundedThreshold = 4f;
-    [SerializeField] private bool BlockAirControl;
+    public float GoUpForce = 12500f;
+    public float ForwardForce = 20000f;
+    public float RotationTorque = 10000f;
+    public Transform[] RaycastHelpers;
+    public Transform CenterRaycastHelper;
+    public float HeightFromGround = 2f;
+    public float GroundedThreshold = 4f;
+    public bool BlockAirControl;
 
-    [SerializeField] private LayerMask GroundLayer;
-    [SerializeField] private Camera Camera;
-    
+    public LayerMask GroundLayer;
+
+    Rigidbody hoverBody;
 
     private float horizontal;
     private float vertical;
-    private bool grounded;
-    
-    private Rigidbody hoverRB;
 
-    private void Awake()
-    {
-        _input = GetComponent<InputHandler>();
-    }
+    bool grounded = false;
 
-    private void Start ()
+	void Start ()
     {
-        hoverRB = GetComponent<Rigidbody>();
-    }
+        hoverBody = GetComponent<Rigidbody>();
+	}
 
     private void Update()
     {
-        checkGrounded();
-        heightUp();
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
     }
 
-    private void FixedUpdate ()
+    void FixedUpdate ()
     {
+        checkGrounded();
+        heightUp();
+        var targetVector = new Vector3(horizontal, 0, vertical);
         
-        var targetVector = new Vector3(_input.InputVector.x, 0, _input.InputVector.y);
-        
-        var movementVector = MoveTowardTarget(targetVector);
-        
-        RotateTowardMovementVector(movementVector);
+        InputMovement(vertical, horizontal, targetVector);
+        alwaysDown();
 	}
 
     void checkGrounded()
     {
-        var ray = new Ray(CenterRaycastHelper.position, -CenterRaycastHelper.up);
+        Ray ray = new Ray(CenterRaycastHelper.position, -CenterRaycastHelper.up);
         RaycastHit hitInfo;
 
-        grounded = Physics.Raycast(ray, out hitInfo, GroundedThreshold, GroundLayer);
+        if (Physics.Raycast(ray, out hitInfo, GroundedThreshold, GroundLayer))
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
     }
 
     void heightUp()
     {
-        foreach (var raycastHelper in RaycastHelpers)
+        foreach (Transform raycastHelper in RaycastHelpers)
         {
-            var ray = new Ray(raycastHelper.position, -raycastHelper.up);
+            Ray ray = new Ray(raycastHelper.position, -raycastHelper.up);
+            RaycastHit hitInfo;
 
-            if (!Physics.Raycast(ray, out var hitInfo, HeightFromGround, GroundLayer)) continue;
-            var distance = Vector3.Distance(raycastHelper.position, hitInfo.point);
-
-            if(distance < HeightFromGround)
+            if(Physics.Raycast(ray, out hitInfo, HeightFromGround, GroundLayer))
             {
-                hoverRB.AddForceAtPosition(raycastHelper.up * GoUpForce * (1f - distance / HeightFromGround), 
-                    raycastHelper.position, ForceMode.Force);
+                float distance = Vector3.Distance(raycastHelper.position, hitInfo.point);
+
+                if(distance < HeightFromGround)
+                {
+                    hoverBody.AddForceAtPosition(raycastHelper.up * GoUpForce * (1f - distance / HeightFromGround), raycastHelper.position, ForceMode.Force);
+                }
             }
         }
     }
 
+    private void InputMovement(float forward, float side, Vector3 targetVector)
+    {
+        if (grounded || !BlockAirControl)
+        {
+            //hoverBody.AddRelativeForce(Vector3.forward * forward * ForwardForce, ForceMode.Force);
+            hoverBody.AddRelativeForce(Vector3.forward * forward * ForwardForce, ForceMode.Acceleration);
+            hoverBody.AddRelativeTorque(
+                Vector3.up * (RotationTorque * side * (forward == 0 ? 1f : Mathf.Sign(forward))), ForceMode.Force);
+        }
+    }
+
+    private void alwaysDown()
+    {
+        if (gameObject.transform.rotation.x > 34 || gameObject.transform.rotation.x < -34)
+        {
+            Debug.Log("doggy");
+            gameObject.transform.Rotate(0,180,0);
+        }
+    }
+}
+
+
+/*
     private Vector3 MoveTowardTarget(Vector3 targetVector)
     {
-        targetVector = Quaternion.Euler(0, 
-            Camera.gameObject.transform.rotation.eulerAngles.y, 0) * targetVector;
+        //targetVector = Quaternion.Euler(0, Camera.gameObject.transform.rotation.eulerAngles.y, 0) * targetVector;
 
         if (grounded || !BlockAirControl)
         {
-            hoverRB.AddForce(targetVector * ForwardForce, ForceMode.Acceleration);
+            //hoverRB.AddForce(targetVector * ForwardForce, ForceMode.Acceleration);
+            hoverRB.AddRelativeForce(Vector3.forward * targetVector.z * ForwardForce, ForceMode.Force);
         }
         return targetVector;
     }
@@ -97,4 +118,4 @@ public class HoverController : MonoBehaviour
         var rotation = Quaternion.LookRotation(movementDirection);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotationSpeed);
     }
-}
+*/
